815fcd5c2bc5aca6f9287933e413aa9bbe00121d
William Deurwaarder <william.git@xs4all.nl>
2016-01-14 00:33:15 +0100

    Renamed isMoving to isColliding

--- src/nl/marayla/Xara/ElementCollisions/Bounce.java
+++ src/nl/marayla/Xara/ElementCollisions/Bounce.java
@@ -25,7 +25,7 @@ public final class Bounce implements ElementCollision {
         final ElementCollisionData element1,
         final ElementCollisionData element2
     ) {
-        assert element1.isMoving();
+        assert element1.isColliding();
 
         final ElementCollision other = element2.getCollision();
         if (other == Bounce.INSTANCE) {
@@ -35,13 +35,13 @@ public final class Bounce implements ElementCollision {
                 element1.getDirection().reverse(),
                 element2.getIndex(),
                 element2.getElement(),
-                element2.isMoving() ? element2.getDirection().reverse() : element2.getDirection()
+                element2.isColliding() ? element2.getDirection().reverse() : element2.getDirection()
             );
         }
         else if (other == Eat.INSTANCE) {
             return new Field.PlacingOne(
                 (
-                    element2.isMoving() ?
+                    element2.isColliding() ?
                         Field.calculateIndex(element2.getIndex(), element2.getDirection()) :
                         element2.getIndex()
                 ),
